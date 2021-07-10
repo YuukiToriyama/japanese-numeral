eb833426903e50df3359fbe116cc4f85da684e37
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-07-11 04:58:43 +0900

    mod,useの挙動がわからない

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,124 @@
 use regex::Regex;
 use std::collections::HashMap;
 
-//mod converter::{normalize_kanjinum_string, normalize_oldish_string, normalize_zenkakunum_string};
+pub mod converter {
+    use std::iter::FromIterator;
+    fn convert_oldish_char(c: char) -> Option<char> {
+        match c {
+            '零' => Some('〇'),
+            '壱' => Some('一'),
+            '壹' => Some('一'),
+            '弐' => Some('二'),
+            '貳' => Some('二'),
+            '貮' => Some('二'),
+            '参' => Some('三'),
+            '參' => Some('三'),
+            '肆' => Some('四'),
+            '伍' => Some('五'),
+            '陸' => Some('六'),
+            '漆' => Some('七'),
+            '捌' => Some('八'),
+            '玖' => Some('九'),
+            '拾' => Some('十'),
+            '陌' => Some('百'),
+            '佰' => Some('百'),
+            '阡' => Some('千'),
+            '仟' => Some('千'),
+            '萬' => Some('万'),
+            _ => None,
+        }
+    }
+
+    pub fn normalize_oldish_string(input: String) -> String {
+        let mut vec_dst: Vec<char> = vec![];
+        for c in input.chars().collect::<Vec<_>>() {
+            match convert_oldish_char(c) {
+                Some(v) => vec_dst.push(v),
+                None => match c {
+                    '廿' => {
+                        vec_dst.push('二');
+                        vec_dst.push('十');
+                    }
+                    _ => vec_dst.push(c),
+                },
+            }
+        }
+        return String::from_iter(vec_dst);
+    }
+
+    pub fn normalize_kanjinum_string(input: String) -> String {
+        let mut vec_dst: Vec<char> = vec![];
+        for c in input.chars().collect::<Vec<_>>() {
+            let filtered = match c {
+                '〇' => '0',
+                '一' => '1',
+                '二' => '2',
+                '三' => '3',
+                '四' => '4',
+                '五' => '5',
+                '六' => '6',
+                '七' => '7',
+                '八' => '8',
+                '九' => '9',
+                _ => c,
+            };
+            vec_dst.push(filtered);
+        }
+        return String::from_iter(vec_dst);
+    }
+
+    pub fn normalize_zenkakunum_string(input: String) -> String {
+        let mut vec_dst: Vec<char> = vec![];
+        for c in input.chars().collect::<Vec<_>>() {
+            let filtered = match c {
+                '０' => '0',
+                '１' => '1',
+                '２' => '2',
+                '３' => '3',
+                '４' => '4',
+                '５' => '5',
+                '６' => '6',
+                '７' => '7',
+                '８' => '8',
+                '９' => '9',
+                _ => c,
+            };
+            vec_dst.push(filtered);
+        }
+        return String::from_iter(vec_dst);
+    }
+
+    #[test]
+    fn test_normalize_oldish_string() {
+        assert_eq!(
+            normalize_oldish_string(String::from("伍伍カレー")),
+            String::from("五五カレー")
+        );
+        assert_eq!(
+            normalize_oldish_string(String::from("CoCo壱番屋廿日市駅前店")),
+            String::from("CoCo一番屋二十日市駅前店")
+        );
+    }
+    #[test]
+    fn test_normalize_kanjinum_string() {
+        assert_eq!(
+            normalize_kanjinum_string(String::from("一九九九年一〇月")),
+            String::from("1999年10月")
+        );
+    }
+    #[test]
+    fn test_normalize_zenkakunum_string() {
+        assert_eq!(
+            normalize_zenkakunum_string(String::from("２０２１年")),
+            String::from("2021年")
+        );
+    }
+}
 
 /**
  * 漢数字を単位ごとに分割する
  */
-pub fn split_large_number(japanese: String) -> HashMap<&str, i32> {
+pub fn split_large_number(japanese: String) -> HashMap<&'static str, i32> {
     let mut input = japanese;
     let mut parsed_num: HashMap<&str, i32> = HashMap::new();
     // 京、兆、億、万の順でそれぞれの桁についてパースしていく
@@ -14,9 +126,8 @@ pub fn split_large_number(japanese: String) -> HashMap<&str, i32> {
         // 正規表現を使い、◯◯万のような文字列をキャプチャする
         let expression = "(?P<number>.+)".to_string() + unit;
         let reg = Regex::new(&expression).unwrap();
-        let capture = reg.captures(&input);
         let mut remove_part = String::new();
-        let result = match capture {
+        let result = match reg.captures(&input) {
             Some(v) => {
                 remove_part = format!("{}{}", &v["number"], unit);
                 kanjinum_to_int(&v["number"])
@@ -30,12 +141,55 @@ pub fn split_large_number(japanese: String) -> HashMap<&str, i32> {
         // 抜き出したものはHashMapに保存
         parsed_num.insert(unit, result);
     }
+
+    // 一万未満の桁がある場合処理を続ける
+    if input.as_str() != "" {
+        parsed_num.insert("千", kanjinum_to_int(&input));
+    } else {
+        parsed_num.insert("千", 0);
+    }
+
     return parsed_num;
 }
 
 fn kanjinum_to_int(input: &str) -> i32 {
-    return 123;
+    // 入力がすべてアラビア数字で構成されている場合
+    if Regex::new(r"^[0-9]+$").unwrap().is_match(input) {
+        let num: i32 = input.parse().unwrap();
+        return num;
+    }
+
+    let mut number = 0;
+    for (unit, base) in vec![("千", 1000), ("百", 100), ("十", 10)] {
+        let expression = "(?P<number>.*)".to_string() + unit;
+        let reg = Regex::new(&expression).unwrap();
+        let capture = reg.captures(input);
+        let n = match capture {
+            Some(v) => {
+                if Regex::new("^[0-9]+$").unwrap().is_match(&v["number"]) {
+                    let num: i32 = v["number"].parse().unwrap();
+                    return num;
+                } else {
+                    let num: i32 = converter::normalize_kanjinum_string(v["number"].to_string())
+                        .parse()
+                        .unwrap();
+                    return num;
+                }
+            }
+            None => 0,
+        };
+        number = number + n * base;
+    }
+    return number;
 }
+
+#[test]
+fn test_kanjinum_to_int() {
+    assert_eq!(kanjinum_to_int("三千"), 3000);
+    assert_eq!(kanjinum_to_int("千九百一"), 1901);
+    assert_eq!(kanjinum_to_int("九九"), 99);
+}
+
 /*
 const match = kanji.match(reg)
     if (match) {
@@ -47,11 +201,6 @@ const match = kanji.match(reg)
 */
 
 /*
-let unit_small: Vec<(&str, i32)> = vec![
-    ("十", 10),
-    ("百", 100),
-    ("千", 1000)
-];
 let unit_large: HashMap<&str, i64> = vec![
     ("万", 1_0000),
     ("億", 1_0000_0000),
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-mod lib;
-
-fn main() {
-    lib::split_large_number("hogehoge".to_string());
-}
